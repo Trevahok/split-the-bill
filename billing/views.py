6446e31b7320a4bd3f695b2946ce5a9b1f2d56aa
Trevahok <vighneshss@gmail.com>
2018-09-12 22:02:42 +0530

    now is a really good time to kill me

--- billing/views.py
+++ billing/views.py
@@ -13,7 +13,7 @@ def home(request):
             }
     if request.method=='POST':
         d = Debt.objects.get_or_create(from_user=User.objects.get(id =request.POST['from_user']) , to_user=User.objects.get(id =request.POST['to_user']))
-        d.amount+=int(request.POST['amount'])
+        d[0].amount+=int(request.POST['amount'])
         d.save()
     return render(request,'debt.html', context)
 
